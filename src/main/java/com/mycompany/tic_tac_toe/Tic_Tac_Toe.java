889fa699360e096211d2d7c44221e0c3968ee033
IrfanFaiq <93464504+IrfanFaiq@users.noreply.github.com>
2024-04-02 16:51:45 +0800

    Tic Tac Toe tutorial Project

---
+++ src/main/java/com/mycompany/tic_tac_toe/Tic_Tac_Toe.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tic_tac_toe;
+
+/**
+ *
+ * @author ahhaz
+ */
+public class Tic_Tac_Toe {
+
+    public static void main(String[] args) {
+        TicTacToe tictactoe = new TicTacToe();
+    }
+}
